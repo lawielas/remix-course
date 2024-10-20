3a5a66a49c01024a7eaf8a2873955764938dd776
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2024-10-20 15:22:24 +0100

    style index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,12 +2,10 @@ import { Link } from "@remix-run/react";
 
 export default function _index() {
   return (
-    <main>
-      <h1>A better way of keeping track of your notes</h1>
-      <p>Try our early beta and never lose track of your notes again!</p>
-      <p>
-        <Link to={"/notes"}>Try Now!</Link>
-      </p>
+    <main className="bg-gradient-to-b from-purple-600 to-purple-400 h-screen flex flex-col items-center py-10 text-white gap-7">
+      <h1 className="text-3xl font-semibold">A better way of keeping track of your notes</h1>
+      <p className="text-sm tracking-wider">Try our early beta and never lose track of your notes again!</p>
+      <Link to={"/notes"} className="bg-orange-300 py-3 px-6 text-lg text-black rounded-md">Try Now!</Link>
     </main>
   )
 }
