6693a43014da9b7ea06b347401f103dfd61b8a0f
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2024-10-20 15:10:22 +0100

    demo page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "@remix-run/react";
+
 export default function _index() {
   return (
-    <h1>
-      Hello Remix!
-    </h1>
+    <>
+      <h1>
+        Hello Remix!
+      </h1>
+      {/* <a href="/demo">Go to demo page</a> */}
+      <Link to={"/demo"}>Go to demo page</Link>
+    </>
   )
 }
---
+++ app/routes/demo.tsx
@@ -0,0 +1,5 @@
+export default function DemoPage() {
+  return (
+    <div>Demo Page</div>
+  )
+}
